4b744e8fd35e0d0997898f40341a649d49b9befe
Wojtek <wojciech.smajda@gmail.com>
2012-11-23 21:33:46 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WsApp::Application.config.secret_token = 'c29711574f7b457e4cef9b6b7d5712c2c7ffe9a9dcc2a7b2d3873b874609fffa3249437e9ab7c8831f79156251c82fb4257c8159f66dd9aeeb0f6b4f03c9e07b'
