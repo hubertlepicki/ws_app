a4791f204bdc14f9dc1f17cfc1a6a9d2089c666f
Wojtek <wojciech.smajda@gmail.com>
2012-11-30 22:25:00 +0100

    zmiany w modelu users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 WsApp::Application.routes.draw do
  
+  get "users/new"
+
   get "static_pages/contact"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
-
+  
+  match 'signup', to: 'users#new' 
+  
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'  
